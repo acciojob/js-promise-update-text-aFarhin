aa55f94dbd9ef26a58db01260e46da7633f67b78
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-27 11:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-// Get a reference to the output element
-const outputElem = document.getElementById('output');
+//your JS code here. If required.
+function call(){
+	return new Promise(function (resolve,reject) {
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	
+	});
+}
 
-// Create a promise that resolves after 1 second with a string
-const delayPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Hello, world!');
-  }, 1000);
-});
 
-// Handle the result of the promise
-delayPromise
-  .then((result) => {
-    // Update the text of the output element
-    outputElem.innerText = result;
-    // Test if the text of the output element was updated correctly
-    if (outputElem.innerText !== result) {
-      throw new Error('Text not updated correctly');
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+call().then(function (data) {
+	const div = document.getElementById('output')
+    div.innerHTML = data;
+})
