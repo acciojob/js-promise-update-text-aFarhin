9ea6d8608f7477f7d9fa7f062f685958a6b749e7
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-08 22:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 //your JS code here. If required.
-function delayInHello() {
-			return new Promise((resolve) => {
-				setTimeout(() => {
-					resolve('Hello, world!');
-				}, 1000);
-			});
-		}
+// function delayInHello() {
+// 			return new Promise((resolve) => {
+// 				setTimeout(() => {
+// 					resolve('Hello, world!');
+// 				}, 1000);
+// 			});
+// 		}
 
-		delayInHello().then((message) => {
-			const outputElement = document.getElementById('output');
-			outputElement.textContent = message;
-		});
+// 		delayInHello().then((message) => {
+// 			const outputElement = document.getElementById('output');
+// 			outputElement.textContent = message;
+// 		});
+
+function delayInHello() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Hello');
+        }, 1000);
+    });
+}
+
+function addWorld(message) {
+    return new Promise((resolve) => {
+        resolve(message + ', world!');
+    });
+}
+
+delayInHello()
+    .then(addWorld)
+    .then((message) => {
+        const outputElement = document.getElementById('output');
+        outputElement.textContent = message;
+    });
